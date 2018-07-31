c5627c5d042369af57a42c78ed95ee2f3a834b5c
DESKTOP-G38F96I\pircea <mircea.sandru@gosocialdev.eu>
2018-08-01 00:26:03 +0300

    bleat

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Post, Body, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Req, Put, Post, Body, UseGuards, UseInterceptors, FileInterceptor, UploadedFile } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './companies.dto';
-import { Roles } from '../common/decorators/roles.decorator';
-import { AuthGuard } from '../common/guards/auth.guard';
-import { CreatedByInterceptor } from '../common/interceptors/createdby.interceptor';
-import { ValidationPipe } from '../common/pipes/validation.pipe';
+import { Roles } from 'common/decorators/roles.decorator';
+import { AuthGuard } from 'common/guards/auth.guard';
+import { CreatedByInterceptor } from 'common/interceptors/createdby.interceptor';
 
 @Controller('companies')
 @UseGuards(AuthGuard)
@@ -12,12 +11,19 @@ export class CompaniesController {
 
     constructor(private readonly companiesService: CompaniesService){}
 
+    // @Post('/add')
+    // @Roles('admin')
+    // @UseInterceptors(CreatedByInterceptor)
+    // async add(@Body() createCompanyDto){
+    //     console.log(createCompanyDto);
+    //     return this.companiesService.addCompany(createCompanyDto);
+    // }
+
     @Post('/add')
-    @Roles('admin')
-    @UseInterceptors(CreatedByInterceptor)
-    async add(@Body(new ValidationPipe()) createCompanyDto: CreateCompanyDto){
-        return this.companiesService.addCompany(createCompanyDto);
-    }
+@UseInterceptors(FileInterceptor('companyLogo'))
+uploadFile(@UploadedFile() file) {
+  console.log(file);
+}
 
     @Post('/all')
     @Roles('admin')
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -14,17 +14,19 @@ export class CompaniesService {
         @InjectModel('Company') private readonly companyModel: Model<Company>
     ){}
 
-    async addCompany(createCompanyDto: CreateCompanyDto): Promise<Company>{
-        let query = {companyName: createCompanyDto.companyName};
-        let alreadyExists = await this.companyModel.findOne(query);
-        if (alreadyExists) throw new HttpException('Company already exists!', HttpStatus.BAD_REQUEST);
-        let newCompany = new this.companyModel(createCompanyDto);
-        try {
-            let company = await newCompany.save();
-            return company;
-        } catch(e){
-            throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    async addCompany(params): Promise<Company>{
+        //console.log(params);
+        return;
+        // let query = {companyName: createCompanyDto.companyName};
+        // let alreadyExists = await this.companyModel.findOne(query);
+        // if (alreadyExists) throw new HttpException('Company already exists!', HttpStatus.BAD_REQUEST);
+        // let newCompany = new this.companyModel(createCompanyDto);
+        // try {
+        //     let company = await newCompany.save();
+        //     return company;
+        // } catch(e){
+        //     throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        // }
     }
 
     async allCompanies(): Promise<Company[]>{
